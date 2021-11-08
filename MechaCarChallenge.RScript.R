e1c5801e2dd7d028e8295b478e4d349e7a660e79
angelnga <angel-ngan@hotmail.com>
2021-11-08 02:13:37 -0800

    Deliverable 2: Create Visualizations for the Trip Analysis

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -3,3 +3,14 @@ library(dplyr)
 MechaCarMPG <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,MechaCarMPG)
 summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,MechaCarMPG))
+
+
+
+#Deliverable 2: Create Visualizations for the Trip Analysis
+SuspensionCoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- summarize(SuspensionCoil,mean(PSI),median(PSI),var(PSI),sd(PSI))
+
+lot <- group_by(SuspensionCoil,Manufacturing_Lot)
+lot_summary <- summarize(lot,mean(PSI),median(PSI),var(PSI),sd(PSI))
+
+
